0405d95bbd40e1bbec3e8dfe84666aa01ace596f
Ronsistemas <ronsistemas7@gmail.com>
2019-06-10 22:31:51 +0100

    Portfolio con primeras rutas

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   anio: number = new Date().getFullYear();
+  autor = 'Ronsistemas';
+
+
   constructor() { }
 
   ngOnInit() {
