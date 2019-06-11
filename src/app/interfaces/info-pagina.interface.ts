c484283df73d253b5b60daf8ba7f6840355850d5
Ronsistemas <ronsistemas7@gmail.com>
2019-06-11 21:40:25 +0100

    Servicio en construccion

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tumblr?: string;
+  equipo_trabajo?: any[];
+}
